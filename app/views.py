31689ed88d7f23406863aef7637b2eabddc566f0
Reckafella <41087947+reckafella@users.noreply.github.com>
2024-09-03 23:46:51 +0300

    more changes done

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
+import os.path
+
 from django.shortcuts import render
+import json
+from django.conf import settings
+"""
 from django.http import JsonResponse
 from django.urls import reverse
 from django.contrib import messages
 from .helpers import is_ajax
-
+"""
 
 def home_view(request):
     ''' View to render the home page '''
@@ -27,5 +32,11 @@ def blog_view(request):
 
 def projects_view(request):
     ''' View to render the home page '''
+    json_file_path = os.path.join(settings.BASE_DIR, 'app', 'static', 'assets', 'data', 'projects.json')
 
-    return render(request=request, template_name='app/projects.html', status=200)
+    with open(json_file_path, 'r') as fl:
+        projects_data = json.load(fl)
+
+    context = dict(projects=projects_data)
+
+    return render(request=request, template_name='app/projects.html', context=context, status=200)

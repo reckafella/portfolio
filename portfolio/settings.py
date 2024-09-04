86eaf008acc04fd2c99eadf0d1556a048e308fc1
Reckafella <41087947+reckafella@users.noreply.github.com>
2024-09-04 14:13:40 +0300

    fix error causing vercel to crash

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    """'default': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }"""
+    }
 }
 
 

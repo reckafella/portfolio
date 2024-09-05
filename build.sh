d8cf0360e481b099d6c0399cd5d2dcc43283f9de
Reckafella <41087947+reckafella@users.noreply.github.com>
2024-09-05 16:02:06 +0300

    add render.yaml

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# Collect static files
+python manage.py collectstatic --no-input --clear

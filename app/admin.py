c99dcc375deef567bc6cda05f6a6cf61ea912355
Reckafella <41087947+reckafella@users.noreply.github.com>
2024-09-07 01:53:15 +0300

    update forms.py & admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from app.forms import BlogPostAdmin
 from app.models import BlogPost
 
-admin.site.register(BlogPost)
+admin.site.register(BlogPost, BlogPostAdmin)

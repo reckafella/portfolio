633929f51d6675b77be49cc767e867fe218724c1
Ethan Wanyoike <41087947+reckafella@users.noreply.github.com>
2024-09-11 01:37:37 +0300

    register project

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 from app.forms import BlogPostAdmin
-from app.models import BlogPost
+from app.models import BlogPost, Projects
 
-admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(BlogPost, Projects, BlogPostAdmin)

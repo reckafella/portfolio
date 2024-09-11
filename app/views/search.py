9cace6e67f0169aa432d9c6fba465d054211ddda
Ethan Wanyoike <41087947+reckafella@users.noreply.github.com>
2024-09-11 23:41:10 +0300

    implement search and pagination for posts

--- app/views/search.py
+++ app/views/search.py
@@ -1,13 +1,57 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.db.models import Q
+
+from app.helpers import is_ajax
+from app.models import BlogPost, Projects
+
+def search_view(request):
+    """ view to handle search """
+    query = request.GET.get('q', '')
+    category = request.GET.get('category', 'all')
+
+    post_results = BlogPost.objects.none()
+    project_results = Projects.objects.none()
+
+    if query:
+        if category in ['all', 'posts']:
+            post_results = BlogPost.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
+            )
+        if category in ['all', 'projects']:
+            project_results = Projects.objects.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            )
+
+    if is_ajax(request):
+        response = {
+            'success': True,
+            'message': f'Search results: {query}',
+            'redirect_url': reverse('search'),
+        }
+        return JsonResponse(response)
+    
+    context = {
+        'query': query,
+        'category': category,
+        'posts': post_results,
+        'projects': project_results,
+        'page_title': 'Search'
+    }
+
+    return render(request, 'app/search.html', context)
+
+""" from django.http import JsonResponse
+from django.shortcuts import render
+from django.urls import reverse
 
 from app.helpers import is_ajax
 from app.models import BlogPost, Projects
 
 
 def search_view(request):
-    """ view to handle search """
+    ''' view to handle search '''
     if request.method == 'POST':
         query = request.POST.get('q')
     elif request.method == 'GET':
@@ -35,3 +79,4 @@ def search_view(request):
     }
 
     return render(request, 'app/search.html', context)
+ """

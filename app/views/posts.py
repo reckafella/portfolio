dcc853521d22da5eedc473142a3b6c4ea1e245ba
Ethan Wanyoike <41087947+reckafella@users.noreply.github.com>
2024-09-14 02:45:42 +0300

    add more features

--- app/views/posts.py
+++ app/views/posts.py
@@ -3,9 +3,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils.text import slugify
-from ..helpers import is_ajax
+from app.helpers import is_ajax
 from django.urls import reverse
 
 from app.forms import BlogPostForm
@@ -74,9 +75,108 @@ def create_post(request):
     return render(request, 'blog/create_post.html', context)
 
 
+
+
 def posts_list_view(request):
-    """ View to render the page listing blog posts """
-    blog_post_list = BlogPost.objects.all().order_by('-created_at')
+    topic = request.GET.get('topic', 'all')
+    
+    if topic == 'all':
+        blog_post_list = BlogPost.objects.all().order_by('-created_at')
+    else:
+        blog_post_list = BlogPost.objects.filter(topics__icontains=topic).order_by('-created_at')
+    
+    all_topics = set()
+    for post in BlogPost.objects.all():
+        all_topics.update(post.get_topics())
+    all_topics = sorted(list(all_topics))
+
+    paginator = Paginator(blog_post_list, 6)
+    page = request.GET.get('page')
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
+    context = {
+        'articles': articles,
+        'page_title': 'Blog Posts',
+        'submit_text': 'Read Article',
+        'topics': all_topics,
+        'current_topic': topic
+    }
+
+    if is_ajax(request):
+        posts_html = render_to_string('blog/partials/posts_list.html', context)
+        pagination_html = render_to_string('blog/partials/pagination.html', context)
+        return JsonResponse({
+            'posts_html': posts_html,
+            'pagination_html': pagination_html,
+        })
+
+    return render(request=request, template_name='blog/posts_list.html', context=context, status=200)
+
+
+def author_posts_view(request, username):
+    author = get_object_or_404(User, username=username)
+    topic = request.GET.get('topic', 'all')
+    
+    if topic == 'all':
+        posts_list = BlogPost.objects.filter(author=author).order_by('-created_at')
+    else:
+        posts_list = BlogPost.objects.filter(author=author, topics__icontains=topic).order_by('-created_at')
+    
+    all_topics = set()
+    for post in BlogPost.objects.filter(author=author):
+        all_topics.update(post.get_topics())
+    all_topics = sorted(list(all_topics))
+
+    paginator = Paginator(posts_list, 6)
+    page = request.GET.get('page')
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
+    context = {
+        'author': author,
+        'articles': articles,
+        'topics': all_topics,
+        'current_topic': topic,
+        'submit_text': 'Read Article',
+    }
+
+    if is_ajax(request):
+        posts_html = render_to_string('blog/partials/posts_list.html', context)
+        pagination_html = render_to_string('blog/partials/pagination.html', context)
+        return JsonResponse({
+            'posts_html': posts_html,
+            'pagination_html': pagination_html,
+        })
+
+    return render(request, 'blog/author_posts.html', context)
+
+""" 
+def posts_list_view(request):
+    ''' View to render the page listing blog posts '''
+    topic = request.GET.get('topic', 'all')
+    
+    if topic == 'all':
+        blog_post_list = BlogPost.objects.all().order_by('-created_at')
+    else:
+        blog_post_list = BlogPost.objects.filter(topics__icontains=topic).order_by('-created_at')
+    
+    # Get all unique topics
+    all_topics = set()
+    for post in BlogPost.objects.all():
+        all_topics.update(post.get_topics())
+    all_topics = sorted(list(all_topics))
+
     paginator = Paginator(blog_post_list, 6)  # Show 6 posts per page
     page = request.GET.get('page')
 
@@ -92,11 +192,48 @@ def posts_list_view(request):
     context = {
         'articles': articles,
         'page_title': 'Blog Posts',
-        'submit_text': 'Read Article'
+        'submit_text': 'Read Article',
+        'topics': all_topics,
+        'current_topic': topic
     }
 
     return render(request=request, template_name='blog/posts_list.html', context=context, status=200)
 
+
+def author_posts_view(request, username):
+    author = get_object_or_404(User, username=username)
+    topic = request.GET.get('topic', 'all')
+    
+    if topic == 'all':
+        posts_list = BlogPost.objects.filter(author=author).order_by('-created_at')
+    else:
+        posts_list = BlogPost.objects.filter(author=author, topics__icontains=topic).order_by('-created_at')
+    
+    # Get all unique topics for this author's posts
+    all_topics = set()
+    for post in BlogPost.objects.filter(author=author):
+        all_topics.update(post.get_topics())
+    all_topics = sorted(list(all_topics))
+
+    paginator = Paginator(posts_list, 6)  # Show 6 posts per page
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
+    context = {
+        'author': author,
+        'posts': posts,
+        'topics': all_topics,
+        'current_topic': topic
+    }
+    return render(request, 'blog/author_posts.html', context)
 def author_posts_view(request, username):
     author = get_object_or_404(User, username=username)
     posts_list = BlogPost.objects.filter(author=author).order_by('-created_at')
@@ -117,3 +254,28 @@ def author_posts_view(request, username):
         'posts': posts
     }
     return render(request, 'blog/author_posts.html', context)
+
+
+def posts_list_view(request):
+    View to render the page listing blog posts
+    blog_post_list = BlogPost.objects.all().order_by('-created_at')
+    paginator = Paginator(blog_post_list, 6)  # Show 6 posts per page
+    page = request.GET.get('page')
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        articles = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page of results.
+        articles = paginator.page(paginator.num_pages)
+
+    context = {
+        'articles': articles,
+        'page_title': 'Blog Posts',
+        'submit_text': 'Read Article'
+    }
+
+    return render(request=request, template_name='blog/posts_list.html', context=context, status=200)
+ """
--- app/views/search.py
+++ app/views/search.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.urls import reverse
 from django.db.models import Q
@@ -6,8 +7,66 @@ from django.db.models import Q
 from app.helpers import is_ajax
 from app.models import BlogPost, Projects
 
+
 def search_view(request):
     """ view to handle search """
+    query = request.GET.get('q', '')
+    category = request.GET.get('category', 'all')
+    page = request.GET.get('page', 1)
+    posts_per_page = 6  # You can adjust this number
+    projects_per_page = 6  # You can adjust this number
+
+    post_results = BlogPost.objects.none()
+    project_results = Projects.objects.none()
+
+    if query:
+        if category in ['all', 'posts']:
+            post_results = BlogPost.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
+            )
+        if category in ['all', 'projects']:
+            project_results = Projects.objects.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            )
+
+    # Pagination for posts
+    post_paginator = Paginator(post_results, posts_per_page)
+    try:
+        paginated_posts = post_paginator.page(page)
+    except PageNotAnInteger:
+        paginated_posts = post_paginator.page(1)
+    except EmptyPage:
+        paginated_posts = post_paginator.page(post_paginator.num_pages)
+
+    # Pagination for projects
+    project_paginator = Paginator(project_results, projects_per_page)
+    try:
+        paginated_projects = project_paginator.page(page)
+    except PageNotAnInteger:
+        paginated_projects = project_paginator.page(1)
+    except EmptyPage:
+        paginated_projects = project_paginator.page(project_paginator.num_pages)
+
+    if is_ajax(request):
+        response = {
+            'success': True,
+            'message': f'Search results: {query}',
+            'redirect_url': reverse('search'),
+        }
+        return JsonResponse(response)
+    
+    context = {
+        'query': query,
+        'category': category,
+        'posts': paginated_posts,
+        'projects': paginated_projects,
+        'page_title': 'Search'
+    }
+    return render(request, 'app/search.html', context)
+
+
+""" def search_view(request):
+    '''view to handle search '''
     query = request.GET.get('q', '')
     category = request.GET.get('category', 'all')
 
@@ -41,8 +100,7 @@ def search_view(request):
     }
 
     return render(request, 'app/search.html', context)
-
-""" from django.http import JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 

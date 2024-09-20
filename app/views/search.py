16d5795bb8acd323a68ff1e6fe78715765c9c28c
Reckafella <41087947+reckafella@users.noreply.github.com>
2024-09-20 12:19:51 +0300

    fix pagination error

--- app/views/search.py
+++ app/views/search.py
@@ -3,10 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.urls import reverse
 from django.db.models import Q
-
 from app.helpers import is_ajax
 from app.models import BlogPost, Projects
-
 
 def search_view(request):
     """ view to handle search """
@@ -15,7 +13,6 @@ def search_view(request):
     page = request.GET.get('page', 1)
     posts_per_page = 6  # You can adjust this number
     projects_per_page = 6  # You can adjust this number
-
     post_results = BlogPost.objects.none()
     project_results = Projects.objects.none()
 
@@ -23,11 +20,12 @@ def search_view(request):
         if category in ['all', 'posts']:
             post_results = BlogPost.objects.filter(
                 Q(title__icontains=query) | Q(content__icontains=query)
-            )
+            ).order_by('-created_at')  # Add ordering here
+
         if category in ['all', 'projects']:
             project_results = Projects.objects.filter(
                 Q(title__icontains=query) | Q(description__icontains=query)
-            )
+            ).order_by('-created_at')  # Add ordering here
 
     # Pagination for posts
     post_paginator = Paginator(post_results, posts_per_page)
@@ -63,78 +61,3 @@ def search_view(request):
         'page_title': 'Search'
     }
     return render(request, 'app/search.html', context)
-
-
-""" def search_view(request):
-    '''view to handle search '''
-    query = request.GET.get('q', '')
-    category = request.GET.get('category', 'all')
-
-    post_results = BlogPost.objects.none()
-    project_results = Projects.objects.none()
-
-    if query:
-        if category in ['all', 'posts']:
-            post_results = BlogPost.objects.filter(
-                Q(title__icontains=query) | Q(content__icontains=query)
-            )
-        if category in ['all', 'projects']:
-            project_results = Projects.objects.filter(
-                Q(title__icontains=query) | Q(description__icontains=query)
-            )
-
-    if is_ajax(request):
-        response = {
-            'success': True,
-            'message': f'Search results: {query}',
-            'redirect_url': reverse('search'),
-        }
-        return JsonResponse(response)
-    
-    context = {
-        'query': query,
-        'category': category,
-        'posts': post_results,
-        'projects': project_results,
-        'page_title': 'Search'
-    }
-
-    return render(request, 'app/search.html', context)
-from django.http import JsonResponse
-from django.shortcuts import render
-from django.urls import reverse
-
-from app.helpers import is_ajax
-from app.models import BlogPost, Projects
-
-
-def search_view(request):
-    ''' view to handle search '''
-    if request.method == 'POST':
-        query = request.POST.get('q')
-    elif request.method == 'GET':
-        query = request.GET.get('q')
-    if query:
-        post_results = BlogPost.objects.filter(title__icontains=query) | BlogPost.objects.filter(content__icontains=query)
-        project_results = Projects.objects.filter(title__icontains=query) | Projects.objects.filter(description__icontains=query)
-    else:
-        post_results = BlogPost.objects.all()
-        project_results = Projects.objects.all()
-
-    if is_ajax(request):
-        response = {
-            'success': True,
-            'message': f'Search results: {query}',
-            'redirect_url': reverse('search'),
-        }
-        return JsonResponse(response)
-    
-    context = {
-        'query': query,
-        'posts': post_results,
-        'projects': project_results,
-        'page_title': 'Search'
-    }
-
-    return render(request, 'app/search.html', context)
- """

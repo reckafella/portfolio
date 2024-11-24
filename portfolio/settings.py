7436336696ea20b3f1b9f65d76bef85fa4c84770
Reckafella <41087947+reckafella@users.noreply.github.com>
2024-11-24 23:51:15 +0300

    update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default=FALLBACK_SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', '.ethanmuthoni.me',
                  'ethanmuthoni.tech', 'vercel.app', 'now.sh']
 

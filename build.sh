31945739cfcaa1ff1de78acc149d95f69799b058
Ethan Wanyoike <41087947+reckafella@users.noreply.github.com>
2024-11-30 08:46:10 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -9,19 +9,16 @@ python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
 
 # Apply database migrations
-python3 manage.py makemigrations --verbosity 3
-python3 manage.py makemigrations app --verbosity 3
-python3 manage.py makemigrations blog --verbosity 3
+python3 manage.py makemigrations
+python3 manage.py makemigrations app
+python3 manage.py makemigrations blog
 
-python3 manage.py migrate --verbosity 3
-python3 manage.py migrate app --verbosity 3
-python3 manage.py migrate blog --verbosity 3
+python3 manage.py migrate
+python3 manage.py migrate app
+python3 manage.py migrate blog
 
 # create superuser
 python3 ./createsuperuser.py
 
 # populate db
 #python3 ./add_projects.py
-
-# run autopep8 to fix code style
-autopep8 --in-place --aggressive --aggressive --recursive .

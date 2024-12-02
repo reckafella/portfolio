abaf3d9d21a013a27276b295de414492769d9b78
Ethan Wanyoike <41087947+reckafella@users.noreply.github.com>
2024-12-02 03:38:54 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os.path
 from pathlib import Path
 
-from sympy import true
-
+from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

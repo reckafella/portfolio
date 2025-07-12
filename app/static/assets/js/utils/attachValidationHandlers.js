ae0198e9f21c902b87df646f440881aaefeb1eb5
Ethan Wanyoike <ethanmuthoniw6@gmail.com>
2025-07-12 16:56:05 +0300

    Update attachValidationHandlers.js

--- app/static/assets/js/utils/attachValidationHandlers.js
+++ app/static/assets/js/utils/attachValidationHandlers.js
@@ -1,1 +1,20 @@
-export default function attachValidationHandlers () 
+function attachValidationHandlers(fieldId, config) {
+    const field = document.getElementById(fieldId);
+    if (!field) return;
+
+    const updateCharCount = () => updateCharacterCount(fieldId, config, validationErrors, updateSubmitButton);
+    updateCharCount();
+
+    field.addEventListener('input', updateCharCount);
+    field.addEventListener('paste', () => setTimeout(updateCharCount, 10));
+    field.addEventListener('keyup', updateCharCount);
+
+    if (typeof config.validate === 'function') {
+        const validator = () => config.validate(fieldId, validationErrors, updateSubmitButton);
+        validator();
+
+        field.addEventListener('input', validator);
+        field.addEventListener('blur', validator);
+        field.addEventListener('paste', () => setTimeout(validator, 10));
+    }
+}

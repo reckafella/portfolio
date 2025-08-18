45aae71b2a870e00a8d8909ee372c214e8b6a9e3
Ethan Wanyoike <ethanmuthoni@gmail.com>
2025-08-18 04:01:58 +0300

    refactor: Restructure project files by removing old index.html and main.tsx, and adding new index.css and index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+// import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(<App />);
